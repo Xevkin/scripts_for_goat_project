f2f0e2b7626de5ebe7ba7551ba7fae3266ec68dc
Kevin_D <dalyk1@tcd.ie>
2015-03-12 16:09:15 +0000

    Made changes to miseq script to deal with floating points, also added fasta to fastq file

---
+++ fastq_to_fasta.py
@@ -0,0 +1,4 @@
+from Bio import SeqIO 
+
+count = SeqIO.convert("KD004-2_trimmed.fastq", "fastq", "KD004-2_trimmed.fa", "fasta")
+
---
+++ index.sh
@@ -0,0 +1,6 @@
+gunzip cow_bosTau8/bosTau8.fa.gz
+bowtie-build cow_bosTau8/bosTau8.fa bosTau8
+gunzip dog_canFam3/canFam3.fa.gz
+bowtie-build dog_canFam3/canFam3.fa canFam3
+bowtie-build goat_CHIR1_0/goat_CHIR1_0.fasta goat_CHIR1_0
+bowtie-build sheep_oviAri3/oviAri3.fa oviAri3
