9b3a6fb5fc4b2734816a7c8e99a9e41f497e9ce6
Kevin_D <dalyk1@tcd.ie>
2015-06-16 12:02:25 +0100

    Update to pipeline

--- merge_MG2_hiseq_bams.sh
+++ merge_MG2_hiseq_bams.sh
@@ -1,10 +1,13 @@
 #this script is to merge the MG2 sample bams following meyer alignment
 #bam is then indexed and DepthOfCoverage is run - large file produced is then removed
 java -jar /research/picard-tools-1.119/MergeSamFiles.jar \
-INPUT=../../MG2_2hs_q25_RG.bam \
+INPUT=../../second_hiseq/MG2/MG2_2hs_q25_RG.bam \
 INPUT=../../first_hiseqs/MG2/MG2_hs1_q25.bam \
 INPUT=/d3/giant_deer_data/MG2_deer_q25_HC_meyer_F4_fixed_RGs.bam \
 OUTPUT=MG2_merged_q25.bam
+#samtools sort  MG2_merged_q25.bam  MG2_merged_q25_sort.bam
+#gzip MG2_merged_q25.bam 
 samtools index MG2_merged_q25.bam 
 java -jar /research/GenomeAnalysisTK-2.6-5-gba531bd/GenomeAnalysisTK.jar -T DepthOfCoverage -R /deer_wg/deer_HS_data/deer_ref/deer_ref_no_U.fa -I=MG2_merged_q25.bam -o=MG2_merged_DoC
 rm MG2_merged_DoC
+gzip MG2_merged_q25.bam
