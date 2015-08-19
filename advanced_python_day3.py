f874a1ba6a2f0ebe91f96ac985901c56434b19f5
Xevkin <dalyk1@tcd.ie>
2015-08-19 09:40:46 +0100

    Update advanced_python_day3.py

--- advanced_python_day3.py
+++ advanced_python_day3.py
@@ -39,3 +39,44 @@ my_function([1,2,3])
 
 #If we have functions referring to each other, it can be very complicated to
 #find the source of the side effect.
+
+#A function may also depend on a variable outside the function, which can be changed (this is a silly thing to do)
+#This means that the same function may be given the same input but give a different output
+
+#Functions which:
+# - don't have any side effects and
+# - always return the same output for the same input
+#are called pure functions.
+
+#Functions are OBJECTS in python.
+
+
+def print_list_with_function(my_list, my_function): 
+    # the my_function argument is the name of a function
+    for element in my_list: 
+        print(my_function(element)) 
+
+input = ['abc', 'defhij', 'kl'] 
+print_list_with_function(input, len) 
+
+
+#we are passing a variable to the function which represents a function
+#we only pass "len" rather than "len()" to the function
+
+#For one-line functions we can use a shortcut: lambda expressions:
+get_second = lambda(input) : input[1] 
+
+print_list_with_function(input, get_second)
+
+#Or even:
+print_list_with_function(input, lambda(input) : input[1] )
+
+#^In this case we are creating an annonymous function and using it as an argument
+
+#In non-functional (procedural?) code, we describe how to obtain the answer.
+total = 0 
+for i in range(11): 
+    total = total + i 
+print(total) 
+
+#In functional code, we decribe the result we want and let the computer figure out how to calculate it
