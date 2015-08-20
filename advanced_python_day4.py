e2fbd7674adfa6b2e79f5a77de2ee884da13efe1
Xevkin <dalyk1@tcd.ie>
2015-08-20 09:09:12 +0100

    Create advanced_python_day4.py

---
+++ advanced_python_day4.py
@@ -0,0 +1,15 @@
+#https://dl.dropboxusercontent.com/u/2838022/python_courses/eg_ap_2015/index.html
+
+#Session 7 - Exception Handling
+
+o = open('missingfile.txt')
+
+print("starting..")
+print('abc' + 3)
+
+dna = 'atctgcatattgcgtctgatg'
+a_count = dna.count('A') 
+print(a_count)
+
+#When something bad like this happens it's called an exception. 
+#When writing code, we can decide what to do when an exception occurs.
