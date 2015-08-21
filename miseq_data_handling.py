1bfa113cd49bc71d65ff7ef9879ea19d1f3bae8a
Xevkin <dalyk1@tcd.ie>
2015-08-21 14:40:18 +0100

    Update miseq_data_handling.py
    rebuilding to make several loops, each with different functions at different stages
    need to rebuild how the summary statistics are written

--- miseq_data_handling.py
+++ miseq_data_handling.py
@@ -30,150 +30,32 @@ import os
 failures = []
 
 def main(date_of_miseq, meyer, option, RG_file):
-	#take all .fastq.gz files in current directory; print them
-
-	files = []
-
-	files = [file for file in os.listdir(".") if file.endswith(".fastq.gz")] 
 	
-	print "fastq.gz files in current directory:"
+	#run the set up function.#set up will create some output directories
+	#and return variables that will be used in the rest of the script
 	
-	print map(lambda x : x ,files)
-
-	#variables will be initialized here so they can be modified by options 
-
-	#reference genomes
-	reference = "~/goat/miseq/data/reference_genomes/goat_CHIR1_0/goat_CHIR1_0.fasta"
-
-	#Prepare output directory
-
-	out_dir = "~/goat/miseq/results/" + date_of_miseq +  "/"
-
-	call("mkdir " + out_dir, shell=True)
-
-	#allow meyer option to be used
-	meyer_input = meyer.rstrip("\n").lower()
-
-	alignment_option = "bwa aln -t 5 -l 1000 "  
-
-	if (meyer_input == "meyer"):
-		print "Meyer option selected."
-		alignment_option = "bwa aln -t 5 -l 1000 -n 0.01 -o 2 " 
-
-	#turn the reference genome path to the sheep if sheep option is selected
-	option = option.rstrip("\n").lower()
-
-	if (option == "sheep"):
-		print "Sheep alignment selected."
-		reference = "~/goat/miseq/data/reference_genomes/sheep_oviAri3/oviAri3.fa" 
-
-	#define default cut_adapt and fastq screen
-
-	cut_adapt = "cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -O 1 -m 30 "
-
-	fastq_screen = "~/goat/miseq/src/fastq_screen_v0.4.4/fastq_screen --aligner bowtie --outdir ./"
+	files, reference, out_dir, cut_adapt, alignment_option, master_list, sample_list, fastq_screen_option = set_up(date_of_miseq, meyer, option) 
 	
-	#if option is mit, then several changes need to occur
-
-	if (option == "mit"):
-
-		print "Mitochondrial alignment selected."
-
-		cut_adapt = "cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -O 1 -m 25 "
-
-		reference = "~/goat/miseq/data/mit_genomes_fastq_screen/goat_mit/goat_mit.fasta"
-
-		fastq_screen = "~/goat/miseq/src/fastq_screen_v0.4.4/fastq_screen --aligner bowtie --conf ~/goat/miseq/src/fastq_screen_v0.4.4/capture_config/capture.conf --outdir ./"
-
-	#variable for RG file
-	RG_file = RG_file.rstrip("\n")
-
-	#initialize a masterlist that will carry summary stats of each sample
-	master_list = []
-	master_list.append(["Sample", "wc-l", "read_count_raw", "wc-l_trimmed", "trimmed_read_count","raw_reads_aligned", "raw %age endogenous", "rmdup_reads_aligned" ,"rmdup_alignment_percent", "reads_aligned_q25", "percentage_reads_aligned_q25"])
-
-	#cycle through each line in the input file
-	for file in files:
+	#sample is the file root
+	#trim fastq files and produce fastqc files
+	#the masterlist will change each time so it needs be equated to the function
 	
-		current_file = file.strip()
-
-		#need to check if the current file still exists
-		if not os.path.isfile(file):
-			
-			print "This file is currently not in the directory"
-			
-			failures.append(file)
-			
-			continue
-
-		#using call([]) to invoke system
-		#cutadapt with the adaptor already defined
-		#minimum overlap before trimming (-O 1) and minimum sequence length before trimming (-m 30) are also preset
-		#note that the -m 30 option is not appropriate for mitochondrial captures
+	for sample in sample_list:
 		
-		#unzip fastq
-		call("gunzip " + current_file, shell=True)
-		current_file = current_file.split(".")[0] + ".fastq"
+		master_list  = trim_fastq(sample, cut_adapt, master_list, out_dir)
+	
+	#run fastq screen on the samples
 
-		#rename the file
-		#also save the current sample as a variable to be used later
-		split_file = current_file.split(".")[0].split("_")
-		print split_file[0]
-		call("mv " + current_file + " " + split_file[0] + ".fastq",shell=True)
+	map(lambda sample: run_fastq_screen(sample, out_dir, fastq_screen_option), sample_list)
 	
-		current_file = split_file[0] + ".fastq"
+	#at this stage we have our fastq files with adaptors trimmed, fastqc and fastq screen run
+	#we can now move on to the next step: alignment
 	
-		sample = split_file[0].rstrip("\n")
+	#going to align to CHIR1.0, as that what was used for AdaptMap
+	print(alignment_option + reference + " " + trimmed_fastq + " > " + sample + ".sai")
+	call(alignment_option + reference + " " + trimmed_fastq + " > " + sample + ".sai",shell=True)	
 	
-		print "Current samples is: " + sample
-	
-	
-		#initialize variable to carry summary statistics
-		summary = []
-		summary.append(sample)
-	
-		#Get number of lines (and from that reads - divide by four) from raw fastq
-		unzipped_fastq = current_file
-		trimmed_fastq = sample + "_trimmed" + ".fastq" 
-	
-		cmd = "wc -l " + unzipped_fastq + " | cut -f1 -d' '" 
-		summary.append(subprocess.check_output(cmd,shell=True))
-	
-		call(cmd,shell=True)
-	
-		#raw reads
-		raw_read_number = int(subprocess.check_output(cmd,shell=True)) / 4
-		summary.append(raw_read_number)
-	
-		#cut raw fastq files
-		call(cut_adapt + unzipped_fastq + " > " + trimmed_fastq + " 2> " + trimmed_fastq + ".log", shell=True)
-	
-		#grab summary statistics of trimmed file
-		cmd = "wc -l " + trimmed_fastq + "| cut -f1 -d' '"
-       		summary.append(subprocess.check_output(cmd,shell=True))
-       		trimmed_read_number = int(subprocess.check_output(cmd,shell=True)) / 4
-       		summary.append(str(trimmed_read_number))
-
-
-		#run fastqc on trimmed fastq file
-		#first we want to create an output directory if there is none to begin with
-		call("mkdir " +  out_dir + "fastqc/", shell=True)
-		
-		call("fastqc " + unzipped_fastq + " -o " + out_dir + "fastqc/", shell=True)
-		call("fastqc " + trimmed_fastq + " -o " + out_dir + "fastqc/", shell=True)
-
-		#run fastq screen
-		call("mkdir " + out_dir + "fastq_screen/",shell=True)
-		call("mkdir " + out_dir + "fastq_screen/" + sample, shell=True)
-	
-		call(fastq_screen + sample + " " + trimmed_fastq + " --outdir " + out_dir + "fastq_screen/" + sample, shell=True)
-	
-		#at this stage we have our fastq files with adaptors trimmed
-		#we can now move on to the next step: alignment
-		#going to align to CHIR1.0, as that what was used for AdaptMap
-		print(alignment_option + reference + " " + trimmed_fastq + " > " + sample + ".sai")
-		call(alignment_option + reference + " " + trimmed_fastq + " > " + sample + ".sai",shell=True)	
-	
+	map(lambda sample :align(RG_file)
 		#Obtain the appropriate read group from the supplied read group file
 		with open(RG_file) as file:
 
@@ -285,6 +167,186 @@ def main(date_of_miseq, meyer, option, RG_file):
 	call("tail -n " + str(number_of_samples) "output_summary | sort | cat header.txt - > tmp; mv tmp " + output_summary ";rm header.txt tmp",shell=True)
 
 	call("mv " + output_summary + " " + out_dir,shell=True)
+
+
+def set_up(date_of_miseq, meyer, option):
+	#take all .fastq.gz files in current directory; print them
+	files = []
+
+	files = [file for file in os.listdir(".") if file.endswith(".fastq.gz")] 
+	
+	print "fastq.gz files in current directory:"
+	
+	print map(lambda x : x ,files)
+
+	#variables will be initialized here so they can be modified by options 
+
+	#reference genomes
+	reference = "~/goat/miseq/data/reference_genomes/goat_CHIR1_0/goat_CHIR1_0.fasta"
+
+	#Prepare output directory
+
+	out_dir = "~/goat/miseq/results/" + date_of_miseq +  "/"
+
+	call("mkdir " + out_dir, shell=True)
+
+	#allow meyer option to be used
+	meyer_input = meyer.rstrip("\n").lower()
+
+	alignment_option = "bwa aln -t 5 -l 1000 "  
+
+	if (meyer_input == "meyer"):
+		print "Meyer option selected."
+		alignment_option = "bwa aln -t 5 -l 1000 -n 0.01 -o 2 " 
+
+	#turn the reference genome path to the sheep if sheep option is selected
+	option = option.rstrip("\n").lower()
+
+	if (option == "sheep"):
+		print "Sheep alignment selected."
+		reference = "~/goat/miseq/data/reference_genomes/sheep_oviAri3/oviAri3.fa" 
+
+	#define default cut_adapt and fastq screen
+
+	cut_adapt = "cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -O 1 -m 30 "
+
+	fastq_screen = "~/goat/miseq/src/fastq_screen_v0.4.4/fastq_screen --aligner bowtie --outdir ./"
+	
+	#if option is mit, then several changes need to occur
+
+	if (option == "mit"):
+
+		print "Mitochondrial alignment selected."
+
+		cut_adapt = "cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -O 1 -m 25 "
+
+		reference = "~/goat/miseq/data/mit_genomes_fastq_screen/goat_mit/goat_mit.fasta"
+
+		fastq_screen = "~/goat/miseq/src/fastq_screen_v0.4.4/fastq_screen --aligner bowtie --conf ~/goat/miseq/src/fastq_screen_v0.4.4/capture_config/capture.conf --outdir ./"
+
+	#variable for RG file
+	RG_file = RG_file.rstrip("\n")
+
+	#initialize a masterlist that will carry summary stats of each sample
+	master_list = []
+	master_list.append([["Sample", "wc-l", "read_count_raw", "wc-l_trimmed", "trimmed_read_count","raw_reads_aligned", "raw %age endogenous", "rmdup_reads_aligned" ,"rmdup_alignment_percent", "reads_aligned_q25", "percentage_reads_aligned_q25"]])
+
+	sample_list = []
+	#cycle through each line in the input file, gunzip
+	for file in files:
+
+		#unzip fastq
+		call("gunzip " + current_file, shell=True)
+		current_file = current_file.split(".")[0] + ".fastq"
+
+		#rename the file
+		#also save the current sample as a variable to be used later
+		split_file = current_file.split(".")[0].split("_")
+		
+		call("mv " + current_file + " " + split_file[0] + ".fastq",shell=True)
+	
+		#current_file = split_file[0] + ".fastq"
+	
+		sample_list.append(split_file[0].rstrip("\n"))
+		
+		for i in sample_list:
+		
+			master_list.append([i])
+
+	return files, reference, out_dir, cut_adapt, alignment_option, master_list, sample_list, fastq_screen
+
+
+def current_file_absent(file, current_step):
+	
+	if not os.path.isfile(file):
+			
+			print "This file is currently not in the directory"
+			
+			failures.append(file + " at the " + current_step)
+			
+			return "continue"
+	
+	
+
+def trim_fastq(current_sample, cut_adapt, master_list, out_dir):
+	
+	print "Current samples is: " + sample
+	
+	unzipped_fastq = current_sample＋".fastq"
+	
+	#check if the current file exists, and if it does not, print an error and move onto the next file
+	if current_file_absent(unzipped_fastq , "trimming fastq"):
+		
+		continue
+
+	#Get number of lines (and from that reads - divide by four) from raw fastq
+	trimmed_fastq = current_sample + "_trimmed" + ".fastq" 
+	cmd = "wc -l " + unzipped_fastq + " | cut -f1 -d' '" 
+	
+	#need to get correct list in the masterlist
+	for i in master_list:
+		
+		if (i[0] == current_sample):
+			
+			i.summary.append(subprocess.check_output(cmd,shell=True))
+			
+			break
+	
+	#call(cmd,shell=True)
+	
+	#raw reads
+	raw_read_number = int(subprocess.check_output(cmd,shell=True)) / 4
+		
+	for i in master_list:
+		
+		if (i[0] == current_sample):
+			
+			i.append(raw_read_number)
+	
+			break
+	
+	#cut raw fastq files
+	call(cut_adapt + unzipped_fastq + " > " + trimmed_fastq + " 2> " + trimmed_fastq + ".log", shell=True)
+	
+	#grab summary statistics of trimmed file
+	cmd = "wc -l " + trimmed_fastq + "| cut -f1 -d' '"
+       	trimmed_read_number = int(subprocess.check_output(cmd,shell=True)) / 4
+       	
+       	for i in master_list:
+		
+		if (i[0] == current_sample):
+			
+			i.append(str(trimmed_read_number))
+	
+			break
+       	
+       	#summary.append(subprocess.check_output(cmd,shell=True))
+       	
+       	#run fastqc on both the un/trimmed fastq files
+	#first we want to create an output directory if there is none to begin with
+	call("mkdir " +  out_dir + "fastqc/", shell=True)
+		
+	call("fastqc " + unzipped_fastq + " -o " + out_dir + "fastqc/", shell=True)
+	call("fastqc " + trimmed_fastq + " -o " + out_dir + "fastqc/", shell=True)
+       	
+       	return master_list
+       	
+def run_fastq_screen(current_sample, out_dir, fastq_screen_option):
+	
+	
+	
+	if current_file_absent(current_sample+"_trimmed.fastq" , "trimming fastq"):
+		
+		print "Sample "＋current_sample＋"is not present during fastq screen"
+		
+		continue
+	
+	call("mkdir " + out_dir + "fastq_screen/",shell=True)
+	
+	call("mkdir " + out_dir + "fastq_screen/" + current_sample, shell=True)
+	
+	call(fastq_screen + current_sample + " " + current_sample + "_trimmed.fastq --outdir " + out_dir + "fastq_screen/" + sample, shell=True)
+	
 
 def process_bam(sample_name):
 		
