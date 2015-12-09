c56b96b1fe91df04eb6ea3457c6310f9fdd9dbf9
Kevin_D <dalyk1@tcd.ie>
2015-12-09 11:15:14 +0000

    updated before initial run

--- hiseq_data_processing.py
+++ hiseq_data_processing.py
@@ -6,8 +6,8 @@ Reads will be trimmed, aligned, read groups added, duplicates removed, then
 Also need to supply a date for the Hiseq run - will automatically make results file
 python script <date_of_hiseq> <meyer> <species> <fastq_screen> <read group file> <directory in which to place output dir/>
 
-read group file needs to be in the follow format: 
-Sample_name\tRGs_to_add (in following format- ID:X\tSM:X\tPL:X\tLB:X)
+read group file needs to be in the follow format (\t are actual tab characters)
+FASTQ_FILE\t@RG\tID:X\tSM:X\tPL:X\tLB:X\tLANE\tSAMPLE_NAME
 ID should be in the format <sample_name>-<macrogen_index_number>-<lane_number>-<hiseq_number>
 LB should refer to PCR: <sample>-<lab index>-<macrogen-index>-<PCR_number>
 
@@ -39,12 +39,12 @@ nuclear_genomes = {
 }
 
 
-def main(date_of_miseq, meyer, species, RG_file, output_dir):
+def main(date_of_hiseq, meyer, species, RG_file, output_dir):
 	
 	#run the set up function.#set up will create some output directories
 	#and return variables that will be used in the rest of the script
 	
-	files, reference, out_dir, cut_adapt, alignment_option, master_list, sample_list, = set_up(date_of_miseq, meyer, species, RG_file, output_dir) 
+	files, reference, out_dir, cut_adapt, alignment_option, master_list, sample_list, = set_up(date_of_hiseq, meyer, species, RG_file, output_dir) 
 	
 	#sample is the file root
 	#trim fastq files and produce fastqc files
@@ -53,11 +53,6 @@ def main(date_of_miseq, meyer, species, RG_file, output_dir):
 	for sample in sample_list:
 		
 		trim_fastq(sample, cut_adapt, out_dir)
-	
-	#run fastq screen on the samples if input variable for fastq_screen is "yes"
-	if (fastq_screen.lower() == "yes"):
-
-		map(lambda sample: run_fastq_screen(sample, out_dir, fastq_screen_option), sample_list)
 	
 	#at this stage we have our fastq files with adaptors trimmed, fastqc and fastq screen run
 	#we can now move on to the next step: alignment
@@ -91,7 +86,7 @@ def main(date_of_miseq, meyer, species, RG_file, output_dir):
 	call("rm *sai*",shell=True)
 	call("mv trimmed_fastq_files_and_logs/ " + out_dir,shell=True)
 
-	output_summary = date_of_miseq + "_summary.table"
+	output_summary = date_of_hiseq + "_summary.table"
 	
 	#print summary stats
 	with open(output_summary, "w") as f:
@@ -137,11 +132,7 @@ def set_up(date_of_miseq, meyer, species, RG_file, output_dir):
 
 	#Prepare output directory
 
-	out_dir = output_dir + date_of_miseq +  "/"
-
-	if (species == "sheep"):
-
-		out_dir = "~/sheep/results/" + date_of_miseq +  "/" 
+	out_dir = output_dir + date_of_hiseq +  "/"
 
 	call("mkdir " + out_dir, shell=True)
 
@@ -170,21 +161,13 @@ def set_up(date_of_miseq, meyer, species, RG_file, output_dir):
 		call("gunzip " + file, shell=True)
 		current_file = file.split(".")[0] + ".fastq"
 
-		#rename the file
-		#also save the current sample as a variable to be used later
-		split_file = current_file.split(".")[0].split("_")
-		
-		call("mv " + current_file + " " + split_file[0] + ".fastq",shell=True)
-	
-		#current_file = split_file[0] + ".fastq"
-	
-		sample_list.append(split_file[0].rstrip("\n"))
+			sample_list.append(current_file.rstrip("\n"))
 	
 	for i in sample_list:
 	
 		master_list.append([i])
 	
-	return files, reference, out_dir, cut_adapt, alignment_option, master_list, sample_list, fastq_screen
+	return files, reference, out_dir, cut_adapt, alignment_option, master_list, sample_list
 
 
 def trim_fastq(current_sample, cut_adapt, out_dir):
@@ -195,8 +178,6 @@ def trim_fastq(current_sample, cut_adapt, out_dir):
 	
 	#Get number of lines (and from that reads - divide by four) from raw fastq
 	trimmed_fastq = current_sample + "_trimmed" + ".fastq" 
-	
-	cmd = "wc -l " + unzipped_fastq + " | cut -f1 -d' '" 
 	
 	#cut raw fastq files
 	call(cut_adapt + unzipped_fastq + " > " + trimmed_fastq + " 2> " + trimmed_fastq + ".log", shell=True)
@@ -231,17 +212,13 @@ def align(sample, RG_file, alignment_option, reference):
                                	print RG
 	file.seek(0)
 
+	#Print the current sample and RG
         print sample
-	print RG                        		
-        call("bwa samse "  + reference + " " + sample + ".sai " + trimmed_fastq + " | samtools view -Sb - > " + sample + ".bam",shell=True)
-	call("cp " + sample + ".bam " +sample+"_cp.bam",shell=True)
-	#add the read groups
-	print "java -jar /research/picard-tools-1.119/AddOrReplaceReadGroups.jar VALIDATION_STRINGENCY=SILENT  INPUT=" + sample + ".bam OUTPUT=" + sample + "_RG.bam " + RG.rstrip("\n")
-	call("java -jar /research/picard-tools-1.119/AddOrReplaceReadGroups.jar VALIDATION_STRINGENCY=SILENT  INPUT=" + sample + ".bam OUTPUT=" + sample + "_RG.bam " + RG.rstrip("\n"), shell=True)
-	
-	print "mv " + sample + "_RG.bam " +  sample + ".bam "
-	call("mv " + sample + "_RG.bam " +  sample + ".bam ",shell=True)	
 
+	print RG                        		
+
+        call("bwa samse -r " + RG.rstrip("\n") + " " + reference + " " + sample + ".sai " + trimmed_fastq + " | samtools view -Sb - > " + sample + ".bam",shell=True)
+	
 def process_bam(sample_name):
 		
 	#sort this bam
@@ -262,23 +239,14 @@ def process_bam(sample_name):
 	#produce q25 bams
 	call("samtools view -b -q25 " + sample_name + "_rmdup_only_aligned.bam >" + sample_name + "_q25.bam",shell=True)
 
-	#sort this bam
-       	call("samtools sort " + sample_name + "_q25.bam " + sample_name + "_q25_sort",shell=True)
-
-       	#remove duplicates from the sorted bam
-       	call("samtools rmdup -s " + sample_name + "_q25_sort.bam " + sample_name + "_q25_rmdup.bam", shell=True)
-
-       	#remove the "sorted with duplicates" bam
-       	call("rm " + sample_name + "_q25_sort.bam",shell=True)
-
-      	#make a copy of the samtools flagstat
-      	call("samtools flagstat " + sample_name + "_q25_rmdup.bam > " + sample_name + "_q25_flagstat.txt",shell=True)
+    	#make a copy of the samtools flagstat
+      	call("samtools flagstat " + sample_name + "_q25.bam > " + sample_name + "_q25.txt",shell=True)
       	
       	#index the q25 bam
-	call("samtools index "+ sample_name + "_q25_rmdup.bam",shell=True)
+	call("samtools index "+ sample_name + "_q25.bam",shell=True)
 	
 	#get idx stats
-	call("samtools idxstats "+ sample_name + "_q25_rmdup.bam > "  + sample_name + ".idx",shell=True)
+	call("samtools idxstats "+ sample_name + "_q25.bam > "  + sample_name + ".idx",shell=True)
 
 
 def get_summary_info(master_list, current_sample):
@@ -343,13 +311,12 @@ def get_summary_info(master_list, current_sample):
 	return master_list
 
 try:
-	date_of_miseq  = sys.argv[1]
+	date_of_hiseq  = sys.argv[1]
 	meyer = sys.argv[2]
 	species = sys.argv[3]
-	mit = sys.argv[4]
-	fastq_screen = sys.argv[5]
-	RG_file  = sys.argv[6]
-	output_dir = sys.argv[7]
+	RG_file  = sys.argv[4]
+	output_dir = sys.argv[5]
+
 except IndexError:
 	print "Incorrect number of variables have been provided"
 	print "Input variables are date_of_miseq, meyer, species, RG_file"
