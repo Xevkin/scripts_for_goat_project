7f9a312e3e2328b3a42c0c0d04d2efae24db0ba9
Kevin_D <dalyk1@tcd.ie>
2015-12-22 16:23:24 +0000

    Quick and dirty update to make script actually work

--- hiseq_data_processing.py
+++ hiseq_data_processing.py
@@ -4,7 +4,7 @@ Script is run in a directory with bam files to be aligned to either goat, wild g
 Reads will be trimmed, aligned, read groups added, duplicates removed, then 
 
 Also need to supply a date for the Hiseq run - will automatically make results file
-python script <date_of_hiseq> <meyer> <species> <trim> <read group file> <directory in which to place output dir/>
+python script <date_of_hiseq> <meyer> <species> <trim> <align> <read group file> <directory in which to place output dir/>
 
 read group file needs to be in the follow format (\t are actual tab characters)
 FASTQ_FILE.GZ\t@RG\tID:X\tSM:X\tPL:X\tLB:X\tLANE\tSAMPLE_NAME
@@ -39,12 +39,12 @@ nuclear_genomes = {
 }
 
 
-def main(date_of_hiseq, meyer, species, trim, RG_file, output_dir):
+def main(date_of_hiseq, meyer, species, trim, align, process, RG_file, output_dir):
 	
 	#run the set up function.#set up will create some output directories
 	#and return variables that will be used in the rest of the script
 	
-	files, reference, out_dir, cut_adapt, alignment_option, master_list, fastq_list = set_up(date_of_hiseq, meyer, species, RG_file, output_dir, trim) 
+	files, reference, out_dir, cut_adapt, alignment_option, fastq_list = set_up(date_of_hiseq, meyer, species, RG_file, output_dir, trim) 
 	
 	#sample is the file root
 	#trim fastq files and produce fastqc files - if we want to
@@ -63,19 +63,20 @@ def main(date_of_hiseq, meyer, species, trim, RG_file, output_dir):
 	#this step will align each fastq and produce raw bams with RGs
 	#they should have the same stem of the initial bam
 
-	map(lambda fastq : align(fastq, RG_file, alignment_option, reference, trim), fastq_list)
+
+	if (align == "yes"):
+ 
+		map(lambda fastq : align(fastq, RG_file, alignment_option, reference, trim), fastq_list)
 
 	#Remove the sai files that we don't care about
 	call("rm *sai",shell=True)
 
 	#sort and remove duplicates from each bam	
-	map(process_bam, fastq_list)
+	if (process == "yes"):
 
-        for sample in fastq_list:
+		map(process_bam, fastq_list)
 
-                master_list = get_summary_info(master_list, sample)
-
-	#now merge the lanes for each sample, and rmdup the merged bams
+        #now merge the lanes for each sample, and rmdup the merged bams
 	merged_bam_list = merge_lanes_and_sample(RG_file)
 	
 	realigned_bam_list = []
@@ -117,23 +118,6 @@ def main(date_of_hiseq, meyer, species, trim, RG_file, output_dir):
 	output_summary = date_of_hiseq + "_summary.table"
 	
 	call("mv *bam* *flagstat* *log" + out_dir,shell=True)
-
-	#print summary stats
-	#with open(output_summary, "w") as f:
-
-	#	writer = csv.writer(f, delimiter='\t', lineterminator='\n')
-	#	writer.writerows(master_list)
-
-	#call("wc -l " + output_summary,shell=True)
-	
-	#number_of_samples = (int((subprocess.check_output("wc -l " + output_summary,shell=True).split(" ")[0])) - 1)
-	
-	#call("head -n1 " + output_summary + "> header.txt ",shell=True)
-	
-	#call("tail -n " + str(number_of_samples) + " " + output_summary + " | sort | cat header.txt - > tmp; mv tmp " + output_summary + ";rm header.txt tmp",shell=True)
-
-	#call("mv " + output_summary + " " + out_dir,shell=True)
-
 
 def set_up(date_of_hiseq, meyer, species, RG_file, output_dir, trim):
 	#take all .fastq.gz files in current directory; print them
@@ -190,9 +174,6 @@ def set_up(date_of_hiseq, meyer, species, RG_file, output_dir, trim):
 	#variable for RG file
 	RG_file = RG_file.rstrip("\n")
 
-	#initialize a masterlist that will carry summary stats of each sample
-	master_list = [["Sample", "read_count_raw", "trimmed_read_count","raw_reads_aligned", "raw %age endogenous", "rmdup_reads_remaining","rmdup_reads_aligned" ,"rmdup_alignment_percent"]]
-
 	fastq_list = []
 	
 	#cycle through each line in the input file, gunzip
@@ -204,11 +185,7 @@ def set_up(date_of_hiseq, meyer, species, RG_file, output_dir, trim):
 
 		fastq_list.append(current_file.rstrip("\n"))
 	
-	for i in fastq_list:
-	
-		master_list.append([i])
-	
-	return files, reference, out_dir, cut_adapt, alignment_option, master_list, fastq_list
+	return files, reference, out_dir, cut_adapt, alignment_option, fastq_list
 
 
 def trim_fastq(current_sample, cut_adapt, out_dir):
@@ -231,6 +208,8 @@ def align(sample, RG_file, alignment_option, reference, trim):
     if (trim != "yes"):
 
     	trimmed_fastq = sample + ".fastq"
+
+	sample = "_".join(sample.split("_")[:-1]) 
 
     print(alignment_option + reference + " " + trimmed_fastq + " > " + sample + ".sai")
     call(alignment_option + reference + " " + trimmed_fastq + " > " + sample + ".sai",shell=True)
@@ -265,10 +244,14 @@ def align(sample, RG_file, alignment_option, reference, trim):
         call("bwa samse -r \'" + RG.rstrip("\n") + "\' " + reference + " " + sample + ".sai " + trimmed_fastq + " | samtools view -Sb - > " + sample + ".bam",shell=True)
 	
 def process_bam(sample_name):
-		
+
+	sample_name = "_".join(sample_name.split("_")[:-1])		
+
 	#sort this bam
+	print "samtools sort " + sample_name + ".bam " + sample_name + "_sort"
 	call("samtools sort " + sample_name + ".bam " + sample_name + "_sort",shell=True)
 	
+	print "samtools rmdup -s " + sample_name + "_sort.bam " + sample_name + "_rmdup.bam"
 	#remove duplicates from the sorted bam
 	call("samtools rmdup -s " + sample_name + "_sort.bam " + sample_name + "_rmdup.bam", shell=True)
 	
@@ -293,63 +276,6 @@ def process_bam(sample_name):
 	#get idx stats
 	#call("samtools idxstats "+ sample_name + "_q25.bam > "  + sample_name + ".idx",shell=True)
 
-
-def get_summary_info(master_list, current_sample):
-
-	print "Initial master list is:"
-	print master_list
-	to_add = []
-
-	unzipped_fastq = current_sample + ".fastq"
-	
-	trimmed_fastq = current_sample + "_trimmed.fastq"
-	
-	cmd = "wc -l " + unzipped_fastq + " | cut -f1 -d' '" 
-	
-	#raw reads
-	file_length = subprocess.check_output(cmd,shell=True)
-	
-	raw_read_number = int(file_length) / 4
-	
-	to_add.append(raw_read_number)
-	
-	#grab summary statistics of trimmed file
-	cmd = "wc -l " + trimmed_fastq + "| cut -f1 -d' '"
-
-       	file_length = subprocess.check_output(cmd,shell=True)
-
-	trimmed_read_number = int(file_length) / 4
-       	
-	to_add.append(str(trimmed_read_number).rstrip("\n"))
-
-       	#get number of reads aligned without rmdup
-	raw_reads_aligned = subprocess.check_output("samtools flagstat " + current_sample + ".bam |  grep 'mapped (' | cut -f1 -d' '",shell=True)
-	to_add.append(raw_reads_aligned.rstrip("\n"))
-
-	#get %age raw alignment
-	raw_alignment_percentage = ((float(raw_reads_aligned)) * 100)/ float(trimmed_read_number)
-
-	to_add.append(str(raw_alignment_percentage).rstrip("\n"))
-
-	rmdup_reads_remaining = subprocess.check_output("more " + current_sample + "_flagstat.txt | head -n1 | cut -f1 -d' '",shell=True) 
-
-	to_add.append(rmdup_reads_remaining.rstrip("\n"))
-	#get reads that aligned following rmdup
-	rmdup_reads_aligned = subprocess.check_output("more " + current_sample + "_flagstat.txt | grep 'mapped (' | cut -f1 -d' '",shell=True)
-	to_add.append(rmdup_reads_aligned.rstrip("\n"))
-
-  	#capture the alignment percentage of the flagstat file, both no q and q30
-	raw_alignment = subprocess.check_output("more " + current_sample + "_flagstat.txt | grep 'mapped (' | cut -f5 -d' ' | cut -f1 -d'%' | sed 's/(//'", shell=True)
-	to_add.append(raw_alignment.rstrip("\n"))
-	
-	for i in master_list:
-		
-		print i		
-		if (i[0] == current_sample):
-			i.extend(to_add)		
-			break
-		
-	return master_list
 
 
 def merge_lanes_and_sample(RG_file):
@@ -393,10 +319,10 @@ def merge_lanes_and_sample(RG_file):
 
 	#create a list of final merged,rmdup bams that will be returned
 	merged_bam_list = []
-
+	
 	#for each sample, go through each lane for that sample and merge each fastq for that sample
 	for sample in sample_list:
-		
+				
 		merged_lane_list = []
 		
 		for lane in sample[1]:
@@ -408,17 +334,26 @@ def merge_lanes_and_sample(RG_file):
 			with open(RG_file) as r:
 
 				for line in r:
-	
-					if lane == line.split("\t")[2]:
 
+					if (lane == line.split("\t")[2] ) and (sample[0] == line.split("\t")[3].rstrip("\n")):
+						print lane
+						print sample[0]	
+						print line				
 						files_in_lane.append(line.split("\t")[0].split(".")[0] + "_rmdup.bam")
 
 			for bam in files_in_lane:
 
 				merge_cmd = merge_cmd + "INPUT=" + bam + " "
-
-				sample_name = bam.split("-")[1].split("_")[0]
+				
+				if not "-" in bam:
+				
+					sample_name = bam.split("_")[0]
  
+				else:
+			
+					sample_name = bam.split("-").split("_")[0]
+
+
 			sample_lane = sample_name + "_"	+ lane + "_merged"		 
 
 			merge_cmd = merge_cmd + "OUTPUT=" + sample_lane + ".bam 2>" + sample_lane + ".log"
@@ -432,7 +367,7 @@ def merge_lanes_and_sample(RG_file):
 			call("samtools flagstat "+ sample_lane + ".bam >"+ sample_lane + "_flagstat.txt" ,shell=True)
 		
 			#remove duplicates from the merged lane bam
-			cmd = "samtools rmdup -s " + sample_lane + ".bam " + sample_lane + "_rmdup.bam"
+			cmd = "samtools rmdup -s " + sample_lane + ".bam " + sample_lane + "_rmdup.bam 2> " + sample_lane + "_rmdup.log"  
 
 			call(cmd,shell=True)
 
@@ -522,12 +457,14 @@ try:
 	meyer = sys.argv[2]
 	species = sys.argv[3]
 	trim = sys.argv[4]
-	RG_file  = sys.argv[5]
-	output_dir = sys.argv[6]
+	align = sys.argv[5]
+	process = sys.argv[6]
+	RG_file  = sys.argv[7]
+	output_dir = sys.argv[8]
 
 except IndexError:
 	print "Incorrect number of variables have been provided"
-	print "Input variables are date_of_hiseq, meyer, species, trim, RG_file, and the directory to put output directories/files"
+	print "Input variables are date_of_hiseq, meyer, species, trim, process, RG_file, and the directory to put output directories/files"
 	print "Exiting program..."
 	sys.exit()
 
@@ -536,4 +473,4 @@ if not output_dir[-1] == "/":
 
 	output_dir = output_dir + "/"
 
-main(date_of_hiseq, meyer, species, trim, RG_file, output_dir)
+main(date_of_hiseq, meyer, species, trim, align, process, RG_file, output_dir)
