f3b94f6900372d54db680a09618b7a4da161fad0
Kevin_D <dalyk1@tcd.ie>
2016-02-12 10:47:07 +0000

    New scropt to quickly run DoC

---
+++ run_DoC.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#Quick script to run DoC on an indexed bam
+#reference_path input_index_bam
+
+out=$(ls $2 |  cut -f1 -d'_')
+
+java -jar /research/GenomeAnalysisTK-2.6-5-gba531bd/GenomeAnalysisTK.jar \
+-T DepthOfCoverage \
+-R $1 \
+--omitDepthOutputAtEachBase \
+-I $2 \
+-o "DoC_"$out
+
+mkdir "DoC_"$out
+mv "DoC_"$out".*" "DoC_"$out
