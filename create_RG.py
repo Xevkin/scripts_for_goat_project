eb0994ac4af67ba3d7c4252d6be1a18c151d5ade
Kevin <dalyk1@tcd.ie>
2016-05-27 15:32:03 +0100

    Made script that creates read group files

---
+++ create_RG.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+
+#expects single input file
+#input file: for each line: fastq.gz name, sample name,number of indexes, indexes, hiseq number (hiseqX), lane number (laneX), PCR number (PCRX), and platform
+#comma seperated
+
+for line in open(sys.argv[1]):
+
+	entry = line.rstrip("\n").split(",")
+	tube = entry[0]
+	sample = entry[1]
+	index_number = entry[2]
+	last_index = 3 + int(index_number)
+	indexes = entry[3:last_index]
+	hiseq_number = entry[last_index]
+	lane_number = entry[last_index + 1]
+	pcr_number = entry[last_index + 2]
+	platform = entry[last_index + 3]
+
+	for i in indexes:
+
+		print tube + "\t" + r"@RG\tID:" + sample + "-index" + i + "-hiseq" + hiseq_number + "-lane" + lane_number + r"\tSM:" + sample + r"\tPL:" + platform + r"\tID:" + sample + "-index" + i + "-PCR" + pcr_number + "\tlane" + lane_number + "\t" + sample 
