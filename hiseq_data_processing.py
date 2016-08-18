ddf9f1eec90fe40352b2645719d1b312636446f2
Kevin <dalyk1@tcd.ie>
2016-08-18 12:02:04 +0100

    Fixed decircularization

--- hiseq_data_processing.py
+++ hiseq_data_processing.py
@@ -330,7 +330,7 @@ def merge_and_process_mit(RG_file, reference):
 
 				call("angsd -doFasta 2 -i " + bam_root + "_rmdup_q" + QC + ".bam  -doCounts 1 -out " + bam_root + "_angsd-consensus-min" + minD + "_q" + QC + " -setMinDepth " + minD + " -minQ " + QC,shell=True)
 
-				call("gunzip " + bam_root + "_angsd-consensus-" + minD + "_q" + QC + ".fa.gz; decircularize.py "  + bam_root + "_angsd-consensus-min" + minD + "_q" + QC + ".fa > " + bam_root + "_angsd-consensus-min" + minD + "_decirc_q" + QC + ".fa",shell=True)
+				call("gunzip " + bam_root + "_angsd-consensus-min" + minD + "_q" + QC + ".fa.gz; decircularize.py "  + bam_root + "_angsd-consensus-min" + minD + "_q" + QC + ".fa > " + bam_root + "_angsd-consensus-min" + minD + "_decirc_q" + QC + ".fa",shell=True)
 
 		call("mkdir " + bam_root + "; mv *angsd-conse* " + bam_root,shell=True)
 
