44acdf001a63611045063a81a45ae53ea5625c2d
Kevin_D <dalyk1@tcd.ie>
2016-09-14 10:17:11 +0100

    Fixed moving the mitochondria file

--- hiseq_data_processing.py
+++ hiseq_data_processing.py
@@ -199,7 +199,7 @@ def set_up(date_of_hiseq, meyer, species, mit, RG_file, output_dir, trim):
 	#allow meyer option to be used
 	meyer_input = meyer.rstrip("\n").lower()
 
-	alignment_option = "bwa aln -t 12 -l 1024 "  
+	alignment_option = "bwa aln -t 2 -l 1024 "  
 
 	if (meyer_input == "meyer"):
 		
@@ -695,7 +695,7 @@ def clean_up(out_dir):
 def clean_up_mit(mitochondrial_references_file,out_dir):
 
 	#make output directories and dump files
-	call("mkdir auxillary_files; mv " + mitochondrial_references_file + " auxillary_files",shell=True)
+	call("mkdir auxillary_files; cp " + mitochondrial_references_file + " auxillary_files" ,shell=True)
 	
 	call("gzip *.bam",shell=True) 
 
