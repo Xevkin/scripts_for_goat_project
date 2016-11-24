36e89d37c8aaaff1ca1cd137509e79281ad93d43
Kevin <dalyk1@tcd.ie>
2016-11-24 10:09:56 +0000

    Prints out the bones of the .arp file

--- fasta_to_arp.py
+++ fasta_to_arp.py
@@ -3,6 +3,10 @@ import sys
 sequence = ""
 sample = ""
 
+print "[Profile]\n\nTitle=\"\"\nNbSamples=1\nGenotypicData=0\nDataType=DNA\nLocusSeparator=NONE\nMissingData='N'\n"
+
+print "[Data]\n\n[[Samples]]\n\nSampleName=\"\"\nSampleSize=\nSampleData= {\n\n"
+
 with open(sys.argv[1]) as file:
 
 	for line in file:
@@ -11,7 +15,7 @@ with open(sys.argv[1]) as file:
 
 			if (len(sequence) > 0):
 
-				print sample.replace(">","") + "\t1\t" + sequence
+				print sample.split(" ")[0].replace(">","") + "\t1\t" + sequence
 
 			sample = line.strip()
 
@@ -20,3 +24,7 @@ with open(sys.argv[1]) as file:
 		else:
 
 			sequence = sequence + line.strip()
+
+
+
+print "\n}\n\n[[Structure]]\n\nStructureName=\"\"\nNbGroups=1\nGroup={\n\"\"\n}"
