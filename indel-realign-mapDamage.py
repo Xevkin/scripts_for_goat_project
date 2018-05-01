b03f042ba505c4e9d43958d708b43cf0c591d46c
Kevin Daly <kdaly@pg4.tcd.ie>
2018-05-01 10:46:46 +0100

    Now indexes

--- indel-realign-mapDamage.py
+++ indel-realign-mapDamage.py
@@ -6,9 +6,11 @@ bamroot = sys.argv[1]
 
 ref = sys.argv[2]
 
+call("samtools index -@ 12 " + bamroot + ".bam",shell=True)
+
 call("java -Xmx100g -jar /home/kdaly/programs/GATK/GenomeAnalysisTK.jar -T RealignerTargetCreator -nt 24 -R " + ref + " -I " + bamroot + ".bam -o forIndelRealigner_" + bamroot + ".intervals 2> " + bamroot + "_intervals.log",shell=True)
 
-call("java -Xmx100g -jar /home/kdaly/programs/GATK/GenomeAnalysisTK.jar -T IndelRealigner -R " + ref + " -I " + bamroot + ".bam -targetIntervals forIndelRealigner_"  + bamroot + ".intervals -o " + bamroot + "_realigned.bam  2> "+ bamroot + "_realigned.log",shell=True)
+call("java -Xmx100g -jar /home/kdaly/programs/GATK/GenomeAnalysisTK.jar -T IndelRealigner -R " + ref + " -I " + bamroot + ".bam -targetIntervals forIndelRealigner_"  + bamroot + ".intervals -o " + bamroot + "_realigned.bam  2> " + bamroot + "_realigned.log",shell=True)
 
 call("samtools index -@ 24 " + bamroot + "_realigned.bam",shell=True)
 
