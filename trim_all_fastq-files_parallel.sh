ba2889344f9dbc92ef564d608df56a7f701c4dba
Kevin Daly <kdaly@pg4.tcd.ie>
2018-05-02 10:31:48 +0100

    Some updates

---
+++ trim_all_fastq-files_parallel.sh
@@ -0,0 +1,11 @@
+rm trimlist.sh
+
+for i in $(ls *1.fastq.gz | cut -f1 -d'.'); do
+
+echo cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -O 1 -m 30 "$i".fastq.gz -o "$i"_trimmed.fastq.gz "2>" "$i"_trimmed.log >> trimlist.sh
+
+done
+
+parallel -x $1 -a trimlist.sh
+
+rm trimlist.sh
