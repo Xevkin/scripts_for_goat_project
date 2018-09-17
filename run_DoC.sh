7a547ab4ed3ca260f413ba4cfd55ee01f727ae60
Kevin Daly <kdaly@pg4.tcd.ie>
2018-09-17 16:40:50 +0100

    Now indexes and deletes non-summary files

--- run_DoC.sh
+++ run_DoC.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-#Quick script to run DoC on an indexed bam
-#reference_path input_index_bam
+#Quick script to run DoC on a bam
+#reference_path input_bam
 
 out=$(ls $2 |  cut -f1 -d'_')
+
+samtools index -@ 24 $2
 
 java -jar /home/kdaly/programs/GATK/GenomeAnalysisTK.jar \
 -T DepthOfCoverage \
@@ -12,3 +14,5 @@ java -jar /home/kdaly/programs/GATK/GenomeAnalysisTK.jar \
 -I $2 \
 -o "DoC_"$out \
 -nt 24
+
+for i in $(ls DoC* | grep -v "sample_summary"); do rm $i; done
