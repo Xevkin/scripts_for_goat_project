5520c58801dbe7f9d75028c14f55dcac545e8a66
Kevin Daly <kdaly@pg4.tcd.ie>
2018-10-24 15:47:17 +0100

    int()

--- filter_vcfs_for_depth.py
+++ filter_vcfs_for_depth.py
@@ -1,5 +1,9 @@
 import sys
 
+min=int(sys.argv[2])
+
+max=int(sys.argv[3])
+
 with open(sys.argv[1]) as file:
 
 	for line in file:
@@ -11,21 +15,20 @@ with open(sys.argv[1]) as file:
 		split_vcf_line = line.split("\t")
 
 		new_line = split_vcf_line[0:9]
-		
+
 		for individual in split_vcf_line[9:]:
 
 			DP = int(individual.split(":")[2].rstrip("\n"))
 
 			SP = int(individual.split(":")[3].rstrip("\n"))
 
-			if (DP < 8) or (DP > 26) or (SP > 13):
+			if (DP < min) or (DP > max) or (SP > 13):
 
 				blank_genotype="./."
 
 				new_entry = blank_genotype + ":" + ":".join(individual.split(":")[1:])
 
 				new_line.append(new_entry)
-				
 
 			else:
 
@@ -34,8 +37,4 @@ with open(sys.argv[1]) as file:
 
 		print "\t".join(new_line).rstrip("\n")
 
-				
 
-			
-
-		
