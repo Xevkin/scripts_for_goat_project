b6c437b8787ca2fc91c991769371f900029c7e07
Kevin Daly <kdaly@pg4.tcd.ie>
2020-09-25 13:06:09 +0100

    More fixes

--- correct_index.sh
+++ correct_index.sh
@@ -14,7 +14,7 @@ for j in $(ls *fastq.gz *fq.gz | grep -v "exact") ; do i=`echo $j | cut -f1 -d'.
 
 	INDEX=`zcat ${j} | head -n 40000| grep $PREFIX |rev | sed -e "s/1\///g" |  cut -c1-8 | rev | sort | uniq -c  | awk -v OFS="\t" '$1=$1' | sort -n -k1,1 | tail -n1 | cut -f2 | sed -e "s/://g"`;
 
-	echo "zcat ${j} | grep \"^@*$INDEX[A-Z]?[A-Z]?$\" | cut -f1 -d' ' | cut -f2 -d'@' >> ${i}_exact.txt ; ~/programs/seqtk/seqtk subseq ${j} ${i}_exact.txt | gzip -c - > ${i}_exact.fastq.gz  " >> index_parallel.txt; done
+	echo "zcat ${j} | grep \"@*$INDEX\" | cut -f1 -d' ' | cut -f2 -d'@' >> ${i}_exact.txt ; ~/programs/seqtk/seqtk subseq ${j} ${i}_exact.txt | gzip -c - > ${i}_exact.fastq.gz  " >> index_parallel.txt; done
 
 parallel -a index_parallel.txt -j $1
 
