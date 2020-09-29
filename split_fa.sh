97cea245fd49aa1655793c3b11c71141141194be
Kevin Daly <kdaly@pg4.tcd.ie>
2020-09-29 12:17:02 +0100

    Update

--- split_fa.sh
+++ split_fa.sh
@@ -1,3 +1,3 @@
 OUT=`echo $1 | cut -f1 -d'.'`
 
-j=0; while read i; do if echo $i | grep -q "^>" ; then j=`expr $j + 1`; fi; echo $j >> ${OUT}_${j}; done < $1
+j=0; while read i; do if echo $i | grep -q "^>" ; then j=`expr $j + 1`; echo $i | sed -e "s/>//g" | cut -f1 -d' ' >> ${OUT}_${j} ; continue; fi; echo $j >> ${OUT}_${j}; done < $1
