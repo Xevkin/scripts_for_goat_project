b3bebc4d65f28812ec5743b35a82134b2ea083da
Kevin Daly <kdaly@pg4.tcd.ie>
2020-09-29 12:20:14 +0100

    U

--- split_fa.sh
+++ split_fa.sh
@@ -1,3 +1,3 @@
 OUT=`echo $1 | cut -f1 -d'.'`
 
-j=0; while read i; do if echo $i | grep -q "^>" ; then j=`expr $j + 1`; echo $i | sed -e "s/>//g" | cut -f1 -d' ' >> ${OUT}_${j}.fa ; continue; fi; echo $j >> ${OUT}_${j}.fa; done < $1
+j=0; while read i; do if echo $i | grep -q "^>" ; then j=`expr $j + 1`; NAME=`echo $i | sed -e "s/>//g" |cut -f1 -d' '`; echo $i | cut -f1 -d' ' >> ${OUT}_${NAME}.fa;  continue; fi; echo $i >> ${OUT}_${NAME}.fa; done < $1
