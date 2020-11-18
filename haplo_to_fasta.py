c845a8220bc24998a52078816ea19d5e811371bf
Kevin Daly <kdaly@pg4.tcd.ie>
2020-11-18 10:00:14 +0000

    Fixed to deal with newlines

--- haplo_to_fasta.py
+++ haplo_to_fasta.py
@@ -24,11 +24,12 @@ with open(sys.argv[1]) as FILE:
 
 			for CURRENT_SAMPLE in range(3, SAMPLE_NUM+3):
 
-				call ("echo \">\""  + str(SPLINE[CURRENT_SAMPLE]) + " >> " + TEMP_FILE, shell=True)
+				call ("echo -e \"\\\\n>\""  + str(SPLINE[CURRENT_SAMPLE]) + " >> " + TEMP_FILE, shell=True)
 
 				call("cut -f " + str(CURRENT_SAMPLE + 1) + " " + sys.argv[1] + " | tail -n +2 | tr \'\n\' \' \' | sed -e \"s/ //g\" >> " + TEMP_FILE,shell=True)
 
 			break
 
+call("tail -n +2 " + TEMP_FILE + " > tmp; mv tmp " + TEMP_FILE,shell=True)
 
-call("awk -F \'\' \'{if ($0 ~ /^>/) print; else for(i=1;i<length;i+=70) print substr($0,i,70)  }\' "  +  TEMP_FILE + " > " + OUT_FILE,shell=True)
+call("awk -F \'\' \'{if ($0 ~ /^>/) print; else for(i=1;i<length;i+=70) print substr($0,i,70) }\' "  +  TEMP_FILE + " > " + OUT_FILE,shell=True)
