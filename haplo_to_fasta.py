9abc5c428e66b36ad9131a2817aabe95bd3aae5c
Kevin Daly <kdaly@pg4.tcd.ie>
2020-11-18 10:30:52 +0000

    Update

--- haplo_to_fasta.py
+++ haplo_to_fasta.py
@@ -33,3 +33,5 @@ with open(sys.argv[1]) as FILE:
 call("tail -n +2 " + TEMP_FILE + " > tmp; mv tmp " + TEMP_FILE,shell=True)
 
 call("awk -F \'\' \'{if ($0 ~ /^>/) print; else for(i=1;i<length;i+=70) print substr($0,i,70) }\' "  +  TEMP_FILE + " > " + OUT_FILE,shell=True)
+
+call("rm "+ TEMP_FILE,shell=True)
