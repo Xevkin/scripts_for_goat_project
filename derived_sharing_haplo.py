9ad840328ab66d606e27b6e42c49ee3d28dead17
Kevin Daly <kdaly@pg4.tcd.ie>
2020-12-04 09:02:34 +0000

    Working script up to be useable (if slow)

--- derived_sharing_haplo.py
+++ derived_sharing_haplo.py
@@ -11,21 +11,21 @@ def main():
 	#H1,H2,H3
 	SAMPLES = sys.argv[2].rstrip("\n").split(",")
 
+	#print out the samples in Q for 3 pop test
 	H1 = int(SAMPLES[0])+2
 	H2 = int(SAMPLES[1])+2
 	H3 = int(SAMPLES[2])+2
 
-	NABBA = 0
-	NBABA = 0
-
-	D_LIST = []
+	#lists to store the ABBA BABA sites
+	ABBA = []
+	BABA = []
 
 	#jackknifing
 	#Going with  5Mb JKs
 	#assuming bootstrap regions are found at /home/kdaly/bootstrap.list
+	#create a list storing the bootstrap region information
 
 	JACKKNIFES = []
-
 	with open("/home/kdaly/bootstrap.list") as FILE:
 
 		for LINE in FILE:
@@ -42,6 +42,11 @@ def main():
 
 			JACKKNIFES.append(REGION)
 
+			ABBA.append(0)
+
+			BABA.append(0)
+
+	#go through each position in the haplo file
 	with open(sys.argv[1]) as FILE:
 
 		for LINE in FILE:
@@ -50,6 +55,7 @@ def main():
 
 			SPLINE = LINE.split("\t")
 
+			#skip over header, printing the samples of interest
 			if LINE.startswith("chr"):
 
 				print "H1 is " + SPLINE[H1]
@@ -66,37 +72,99 @@ def main():
 
 				continue
 
-			#get ancestral and derived allele
-			ANCESTRAL = SPLINE[3]
-
-			DERIVED = "".join(SPLINE[4:]).replace(ANCESTRAL,"").replace("N","")[0]
-
+			#get position and chromosome for later
 			CHR = int(SPLINE[0])
 
 			POS = int(SPLINE[1])
 
-			#get ABBA/BABA sites
-			NABBA, NBABA = GET_ABBA_BABA(SPLINE,ANCESTRAL,DERIVED,H1,H2,H3,NABBA, NBABA)
+			#keep jack-knifes estimates
+			#create a counter to loop through each block
+			COUNTER = 0
 
+			#for each line, interate over the jackknife regions
 			for IND_JACKKNIFE in JACKKNIFES:
 
-				if not(CHR == IND_JACKKNIFE[0] and POS >= IND_JACKKNIFE[1] and POS <= IND_JACKKNIFE[2]):
+				#if the position matches the current jk region
+				if CHR == IND_JACKKNIFE[0] and POS >= IND_JACKKNIFE[1] and POS <= IND_JACKKNIFE[2]:
 
-					IND_JACKKNIFE[3], IND_JACKKNIFE[4] = GET_ABBA_BABA(SPLINE,ANCESTRAL,DERIVED,H1,H2,H3,IND_JACKKNIFE[3], IND_JACKKNIFE[4])
+					#add to the current ABBA BABA count for the current jk region
+					JACKKNIFES[COUNTER][3],JACKKNIFES[COUNTER][4] = GET_ABBA_BABA(SPLINE,H1,H2,H3,JACKKNIFES[COUNTER][3],JACKKNIFES[COUNTER][4])
 
-				#print "EXCLUDE" + " " + str(CHR) + " " + str(IND_JACKKNIFE[1]) + " " + str(IND_JACKKNIFE[2])
+					#also add to the ABBA BABA lists
+					ABBA[COUNTER] = ABBA[COUNTER] + JACKKNIFES[COUNTER][3]
 
-	print str(NABBA) + " " + str(NBABA) + str(D(NABAB, NBABA))
+					BABA[COUNTER] = BABA[COUNTER] + JACKKNIFES[COUNTER][4]
 
-	#get standard deviation of estimates and print
-	for m in JACKKNIFES:
+					continue
 
-		 D_LIST.append( D(m[3], m[4]))
+				#update the counter to keep track of the current jk region
+				COUNTER += 1
 
-	print np.std(D_LIST)
+	#new counter, for tracking jk regions as we filter out those with no data
+	COUNTER = 0
 
-def GET_ABBA_BABA(SPLINE_IN,ANCESTRAL_IN,DERIVED_IN,H1_IN,H2_IN,H3_IN,NABBA_IN, NBABA_IN):
+	#we want to exclude jk regions with no information
+	TO_INCLUDE = []
 
+	for BLOCK in JACKKNIFES:
+
+		#if missing both ABBA or BABA information, skip
+		if BLOCK[3] != 0 or BLOCK[4] != 0:
+
+			TO_INCLUDE.append(COUNTER)
+
+		#update counter for new jk region
+		COUNTER += 1
+
+	#strip out ABBA / BABA information from jk regions we are keeping; also sum
+	ABBA_TO_INCLUDE = list(map(ABBA.__getitem__,TO_INCLUDE))
+
+	ABBA_TO_INCLUDE_SUM = sum(ABBA_TO_INCLUDE)
+
+	BABA_TO_INCLUDE = list(map(BABA.__getitem__,TO_INCLUDE))
+
+	BABA_TO_INCLUDE_SUM = sum(BABA_TO_INCLUDE)
+
+	#new counter for jk regions we are keeping
+	COUNTER = 0
+
+	#list for each jk estimate of D
+	JACK_D = []
+
+	#for each jk block
+	for BLOCK in ABBA_TO_INCLUDE:
+
+		#"remove" a given region by adding its negative to the list, which will be summed
+		ABBA_SUBSET = ABBA_TO_INCLUDE + [-ABBA_TO_INCLUDE[COUNTER]]
+
+		BABA_SUBSET = BABA_TO_INCLUDE + [-BABA_TO_INCLUDE[COUNTER]]
+
+		#add the jk D estimate to the jk D list
+		JACK_D.append(D(sum(ABBA_SUBSET), sum(BABA_SUBSET) ))
+
+		#update counter for each jk region
+		COUNTER += 1
+
+	#get the total D estimate
+	D_EST = D(ABBA_TO_INCLUDE_SUM, BABA_TO_INCLUDE_SUM)
+
+	#calculate the sd of the jk D estimates
+	JACK_ERR = np.std(JACK_D)
+
+	#calculate Z score of the D estimate
+	Z = D_EST / JACK_ERR
+
+	#print out the total ABBA, BABA sites, D estimates, jk error, and the Z score
+	print " ".join(str(e) for e in [ABBA_TO_INCLUDE_SUM, BABA_TO_INCLUDE_SUM, D_EST, JACK_ERR, Z])
+
+def GET_ABBA_BABA(SPLINE_IN,H1_IN,H2_IN,H3_IN,NABBA_IN, NBABA_IN):
+
+	" Function for taking in a .haplo row and determining if ABBA/BABA count should be added to"
+	ANCESTRAL_IN = SPLINE_IN[3]
+
+	DERIVED_IN = "".join(SPLINE_IN[4:]).replace(ANCESTRAL_IN,"").replace("N","")[0]
+
+	#add to the current ABBA/BABA count when appropriate
 	if SPLINE_IN[H1_IN] == ANCESTRAL_IN and SPLINE_IN[H2_IN] == DERIVED_IN and SPLINE_IN[H2_IN] == SPLINE_IN[H3_IN]:
 
 		NABBA_IN += 1
@@ -108,7 +176,7 @@ def GET_ABBA_BABA(SPLINE_IN,ANCESTRAL_IN,DERIVED_IN,H1_IN,H2_IN,H3_IN,NABBA_IN, 
 	return(NABBA_IN, NBABA_IN)
 
 def D(ABBA, BABA):
-
+	" Function for calculating D statistic "
 	return( (ABBA - BABA) / (ABBA + BABA))
 
 main()
