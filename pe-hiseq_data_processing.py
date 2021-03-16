0a72912f16f2acbc336b0b3aa5c88cbf71715bb6
Kevin Daly <kdaly@pg4.tcd.ie>
2021-03-16 16:16:37 +0000

    Final fix I think

--- pe-hiseq_data_processing.py
+++ pe-hiseq_data_processing.py
@@ -134,7 +134,7 @@ def main(date_of_hiseq, meyer, threads, species, mit, skip_mit_align, trim, alig
         	#do all markdup at same time
 		call("echo Removing duplicates",shell=True)
 
-		call("PIDS_list="";for i in $(ls *sort_q20.bam | grep -v \"_pe_\" | rev | cut -f3- -d'_' | rev ); do echo samtools markdup -r \"$i\"_sort_q20.bam \"$i\"_q20_markdup.bam \"2>\" \"$i\"_q20_markdup.log;  samtools markdup -r \"$i\"_sort_q20.bam \"$i\"_q20_markdup.bam 2> \"$i\"_q20_markdup.log & PIDS_list=`echo $PIDS_list $!`; done; for i in $(ls *resort.bam | grep \"_pe_\" | rev | cut -f3- -d'_' | rev ); do echo samtools markdup -r -@ 6 \"$i\"_sort_q20_fix_resort.bam \"$i\"_q20_markdup.bam \"2>\" \"$i\"_q20_markdup.log; samtools markdup  \"$i\"_sort_q20_fix_resort.bam \"$i\"_q20_markdup.bam 2> \"$i\"_q20_markdup.log & PIDS_list=`echo $PIDS_list $!`; done; for pid in $PIDS_list; do wait $pid; done",shell=True)
+		call("PIDS_list="";for i in $(ls *sort_q20.bam | grep -v \"_pe_\" | rev | cut -f3- -d'_' | rev ); do echo ~/programs/samtools-1.11/samtools markdup -r \"$i\"_sort_q20.bam \"$i\"_q20_markdup.bam \"2>\" \"$i\"_q20_markdup.log;  ~/programs/samtools-1.11/samtools markdup -r \"$i\"_sort_q20.bam \"$i\"_q20_markdup.bam 2> \"$i\"_q20_markdup.log & PIDS_list=`echo $PIDS_list $!`; done; for i in $(ls *resort.bam | grep \"_pe_\" | rev | cut -f3- -d'_' | rev ); do echo ~/programs/samtools-1.11/samtools markdup -r -@ 6 \"$i\"_sort_q20_fix_resort.bam \"$i\"_q20_markdup.bam \"2>\" \"$i\"_q20_markdup.log; ~/programs/samtools-1.11/samtools markdup  \"$i\"_sort_q20_fix_resort.bam \"$i\"_q20_markdup.bam 2> \"$i\"_q20_markdup.log & PIDS_list=`echo $PIDS_list $!`; done; for pid in $PIDS_list; do wait $pid; done",shell=True)
 
 		call("for i in $(ls *markdup.bam | cut -f 1 -d'.'); do samtools flagstat -@ 12 ${i}.bam > ${i}.flagstat; samtools view -@ 12 -b -F 4 $i.bam > tmp.bam; mv tmp.bam $i.bam ;done; rm tmp.bam",shell=True)
 
@@ -167,7 +167,7 @@ def main(date_of_hiseq, meyer, threads, species, mit, skip_mit_align, trim, alig
 
 		sample =  bam.split(".")[0]
 
-		call("echo samtools markdup -r " + bam + " " + sample + "_markdup.bam \">\"  " + sample + "_markdup.log >> markdup.sh",shell=True)
+		call("echo ~/programs/samtools-1.11/samtools markdup -r " + bam + " " + sample + "_markdup.bam \">\"  " + sample + "_markdup.log >> markdup.sh",shell=True)
 
 		merged_markdup_bam_list.append(sample + "_markdup.bam")
 
@@ -376,11 +376,11 @@ def align_process_mit(fastq, RG_file, alignment_option, reference, trim):
 	print "samtools sort -@ 24 " + sample_and_ref + "_pe_mit_F4_sort_fixmate.bam -o " + sample_and_ref + "_pe_mit_F4_sort_fixmate_resort.bam"
 	call("samtools sort -@ 24 " + sample_and_ref + "_pe_mit_F4_sort_fixmate.bam -o " + sample_and_ref + "_pe_mit_F4_sort_fixmate_resort.bam",shell=True)
 
-	print "samtools markdup -r "  + sample_and_ref +"_mit_F4_sort.bam " + sample_and_ref + "_mit_F4_markdup.bam 2>>" + sample_and_ref + "_mit_alignment.log"
-	call("samtools markdup -r "  + sample_and_ref +"_mit_F4_sort.bam " + sample_and_ref + "_mit_F4_markdup.bam 2>> " + sample_and_ref + "_mit_alignment.log",shell=True)
+	print "~/programs/samtools-1.11/samtools markdup -r "  + sample_and_ref + "_mit_F4_sort.bam " + sample_and_ref + "_mit_F4_markdup.bam 2>>" + sample_and_ref + "_mit_alignment.log"
+	call("~/programs/samtools-1.11/samtools markdup -r "  + sample_and_ref +"_mit_F4_sort.bam " + sample_and_ref + "_mit_F4_markdup.bam 2>> " + sample_and_ref + "_mit_alignment.log",shell=True)
 
-	print "samtools markdup -r "  + sample_and_ref +"_pe_mit_F4_sort_fixmate_resort.bam " + sample_and_ref + "_pe_mit_F4_markdup.bam 2>>" + sample_and_ref + "_pe_mit_alignment.log"
-	call("samtools markdup -r "  + sample_and_ref +"_pe_mit_F4_sort_fixmate_resort.bam " + sample_and_ref + "_pe_mit_F4_markdup.bam 2>>" + sample_and_ref + "_pe_mit_alignment.log",shell=True)
+	print "~/programs/samtools-1.11/samtools markdup -r "  + sample_and_ref +"_pe_mit_F4_sort_fixmate_resort.bam " + sample_and_ref + "_pe_mit_F4_markdup.bam 2>>" + sample_and_ref + "_pe_mit_alignment.log"
+	call("~/programs/samtools-1.11/samtools markdup -r "  + sample_and_ref +"_pe_mit_F4_sort_fixmate_resort.bam " + sample_and_ref + "_pe_mit_F4_markdup.bam 2>>" + sample_and_ref + "_pe_mit_alignment.log",shell=True)
 
 	call("rm " + sample_and_ref + "*_mit_F4_sort.bam",shell=True)
 
@@ -406,8 +406,8 @@ def merge_and_process_mit(RG_file, reference, trim):
 		print "samtools flagstat " + bam + "  > " + bam_root + ".flagstat"
 		call("samtools flagstat " + bam + "  > " + bam_root + ".flagstat",shell=True)
 
-		print "samtools markdup -r " + bam_root + ".bam " + bam_root + "_markdup.bam "
-		call("samtools markdup -r " + bam_root + ".bam " + bam_root + "_markdup.bam ",shell=True)
+		print "~/programs/samtools-1.11/samtools markdup -r " + bam_root + ".bam " + bam_root + "_markdup.bam "
+		call("~/programs/samtools-1.11/samtools markdup -r " + bam_root + ".bam " + bam_root + "_markdup.bam ",shell=True)
 
 		print "samtools flagstat " + bam_root + "_markdup.bam > " + bam_root + "_markdup.flagstat"
 		call("samtools flagstat " + bam_root + "_markdup.bam > " + bam_root + "_markdup.flagstat",shell=True)
@@ -663,7 +663,7 @@ def merge_lanes_and_sample(RG_file, trim, species,mit="no", mit_reference="no"):
 
 							if (trim=="no"):
 
-								sample_files.append(line.split("\t")[0].split(".")[0].replace("_r1","")  + mit_reference +  "_mit_F4_markdup.bam")
+								sample_files.append(line.split("\t")[0].split(".")[0].replace("_r1","") + "_"  + mit_reference +  "_mit_F4_markdup.bam")
 
 								sample_files.append(line.split("\t")[0].split(".")[0].replace("_r1","") +  "_pe_mit_F4_markdup.bam")
 
@@ -714,7 +714,7 @@ def merge_lanes_and_sample(RG_file, trim, species,mit="no", mit_reference="no"):
 
 		call("samtools view -b -q 30 -@ 20 " + sample_name + "_q20_merged.bam > " + sample_name + "_merged_q30.bam",shell=True)
 
-		#call("samtools markdup -r " + sample_name + "_merged_q30.bam " + sample_name + "_merged_q30_markdup.bam 2> " + sample_name + "_merged_q30_markdup.log",shell=True)
+		#call("~/programs/samtools-1.11/samtools markdup -r " + sample_name + "_merged_q30.bam " + sample_name + "_merged_q30_markdup.bam 2> " + sample_name + "_merged_q30_markdup.log",shell=True)
 
 		call("samtools flagstat -@ 20 " + sample_name + "_q20_merged.bam > " + sample_name + "_q20_merged.flagstat",shell=True)
 
