7bc1437d957f355d74a6ad2cbd2d174e14f2e4b6
Kevin Daly <kdaly@pg4.tcd.ie>
2021-07-13 11:14:08 +0100

    some extensions

--- sheep-laser.sh
+++ sheep-laser.sh
@@ -2,22 +2,28 @@ BAMFILE=$1
 OUTNAME=`echo $BAMFILE | cut -f1 -d'.'`
 DATASET_PATH=/st1/hdd/pg/kdaly/sheep/datasets/
 REF=/Reference_Genomes/sheep/oviAri3_mod.fa
+LASER=/home/kdaly/.local/bin/laser
+PILEUPS_PATH=/st1/hdd/pg/kdaly/sheep/ancient_Sheep_Bams/pileups/
+#RAND=`echo $RANDOM`; rm parallel${RAND}.sh
+#while read i
+#do
+#SAMPLE=`echo $i | rev | cut -f1 -d'/' | rev | cut -f1 -d'_'`
+#for DATASET in andrew andrew_trans
+#do
+#echo samtools mpileup -q 30 -Q 20 -B -f $REF -l ${DATASET_PATH}/${DATASET}.bed ${i} ">" ${SAMPLE}_${DATASET}.pileup >> parallel${RAND}.sh
+#done
+#done < $BAMFILE
 
-RAND=`echo $RANDOM`; rm parallel${RAND}.sh
-while read i
-do
-SAMPLE=`echo $i | rev | cut -f1 -d'/' | rev | cut -f1 -d'_'`
+#parallel -a  parallel${RAND}.sh -j 12
+
+#rm  parallel${RAND}.sh
+
 for DATASET in andrew andrew_trans
 do
-echo samtools mpileup -q 30 -Q 20 -B -f $REF -l ${DATASET_PATH}/${DATASET}.bed ${i} ">" ${SAMPLE}_${DATASET}.pileup >> parallel${RAND}.sh
-done
-done < $BAMFILE
+python /home/kdaly/programs/LASER-2.04/pileup2seq/pileup2seq.py -f ${REF} -m ${DATASET_PATH}${DATASET}.site -b ${DATASET_PATH}${DATASET}.bed -o ${DATASET}_${OUTNAME} *_${DATASET}.pileup ${PILEUPS_PATH}/*_${DATASET}.pileup
 
-parallel -a  parallel${RAND}.sh -j 12
+echo -e GENO_FILE ${DATASET_PATH}${DATASET}.geno\\nSEQ_FILE ${DATASET}_${OUTNAME}.seq\\nCOORD_FILE\\nOUT_PREFIX ${DATASET}_${OUTNAME}\nDIM 6\\nDIM_HIGH\\nMIN_LOCI 10000 > ${DATASET}_${OUTNAME}.par
 
-rm  parallel${RAND}.sh
+${LASER} -p ${DATASET}_${OUTNAME}.par -r 10
 
-for DATASET in andrew andrew_trans
-do
-python /home/kdaly/programs/LASER-2.04/pileup2seq/pileup2seq.py -f ${REF} -m ${DATASET_PATH}/${DATASET}.site -b ${DATASET_PATH}/${DATASET}.bed -o ${DATASET}_${OUTNAME} *_${DATASET}.pileup
 done
