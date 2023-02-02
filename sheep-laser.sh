dc224f3ffb691d47206b3dabd841936c5c2838b1
Kevin <dalyk1@tcd.ie>
2023-02-02 10:56:19 +0000

    Fixing sheep laser

--- sheep-laser.sh
+++ sheep-laser.sh
@@ -1,9 +1,10 @@
 BAMFILE=$1
 OUTNAME=`echo $BAMFILE | cut -f1 -d'.'`
-DATASET_PATH=/st1/hdd/pg/kdaly/sheep/datasets/
-REF=/Reference_Genomes/sheep/oviAri3_mod.fa
+DATASET_PATH=/raid_md0/kdaly/sheep/datasets/andrew_early2021/
+REF=/raid_md0/Reference_Genomes/sheep/oviAri3_mod.fa
 LASER=/home/kdaly/.local/bin/laser
-PILEUPS_PATH=/st1/hdd/pg/kdaly/sheep/ancient_Sheep_Bams/pileups/
+PILEUPS_PATH=/raid_md0/sheep/bams_oviAri3/pileups/
+LASER_PILEUP_PY=/home/kdaly/programs/LASER/pileup2seq/pileup2seq.py
 RAND=`echo $RANDOM`; rm parallel${RAND}.sh
 
 while read i
@@ -12,11 +13,11 @@ do
 
 SAMPLE=`echo $i | rev | cut -f1 -d'/' | rev | cut -f1 -d'_'`
 
-for DATASET in andrew andrew_trans
+for DATASET in andrew_trans
 
 do
 
-echo samtools mpileup -q 30 -Q 20 -B -f $REF -l ${DATASET_PATH}/${DATASET}.bed ${i} ">" ${SAMPLE}_${DATASET}.pileup >> parallel${RAND}.sh
+echo samtools mpileup -q 30 -Q 20 -B -f $REF -l ${DATASET_PATH}${DATASET}.bed ${i} ">" ${SAMPLE}_${DATASET}.pileup >> parallel${RAND}.sh
 
 done
 
@@ -26,11 +27,11 @@ parallel -a  parallel${RAND}.sh -j 12
 
 rm  parallel${RAND}.sh
 
-for DATASET in andrew andrew_trans
+for DATASET in andrew_trans
 
 do
 
-python /home/kdaly/programs/LASER-2.04/pileup2seq/pileup2seq.py -f ${REF} -m ${DATASET_PATH}${DATASET}.site -b ${DATASET_PATH}${DATASET}.bed -o ${DATASET}_${OUTNAME} *_${DATASET}.pileup ${PILEUPS_PATH}*_${DATASET}.pileup
+python $LASER_PILEUP_PY -f ${REF} -m ${DATASET_PATH}${DATASET}.site -b ${DATASET_PATH}${DATASET}.bed -o ${DATASET}_${OUTNAME} *_${DATASET}.pileup ${PILEUPS_PATH}*_${DATASET}.pileup
 
 echo -e GENO_FILE ${DATASET_PATH}${DATASET}.geno\\nNUMTHREADS 8\\nSEQ_FILE ${DATASET}_${OUTNAME}.seq\\nOUT_PREFIX ${DATASET}_${OUTNAME}\\nDIM 6\\nMIN_LOCI 10000 > ${DATASET}_${OUTNAME}.par
 
@@ -40,6 +41,6 @@ sed -f /home/kdaly/programs/scripts_for_goat_project/fix_sheep.sed ${DATASET}_${
 
 sed -f /home/kdaly/programs/scripts_for_goat_project/fix_sheep.sed ${DATASET}_${OUTNAME}.SeqPC.coord > tmp1 && mv tmp1  ${DATASET}_${OUTNAME}.SeqPC.coord
 
-/home/kdaly/R/bin/Rscript /home/kdaly/programs/kiel_project/sheep_laser_pca.r ${DATASET}_${OUTNAME}.RefPC.coord  ${DATASET}_${OUTNAME}.SeqPC.coord
+Rscript /home/kdaly/raid/kiel/programs/sheep_laser_pca.r ${DATASET}_${OUTNAME}.RefPC.coord  ${DATASET}_${OUTNAME}.SeqPC.coord
 
 done
